0c17acf54addc711dc8fcf3bf25487c78d06810e
Nadjib Mouhoun <79546746+Nadjib-coder@users.noreply.github.com>
2026-07-04 15:54:17 +0100

    test auto deployment

--- backend/test-worker.js
+++ backend/test-worker.js
@@ -4,3 +4,4 @@ const { processPendingArticles } = require("./src/services/signalWorker");
 processPendingArticles({ batchSize: 3, delayMs: 1500 })
   .then(r => { console.log("Result:", r); process.exit(0); })
   .catch(e => { console.error(e); process.exit(1); });
+  
